5208802ef4c48601b09ef8b60229d6017a642408
tinder-christopherperry <christopher.perry@gotinder.com>
2021-05-10 14:24:26 -0700

    add avatar view definition

--- library-ui-core/src/main/java/com/tinder/ui/core/TinderAvatarView.kt
+++ library-ui-core/src/main/java/com/tinder/ui/core/TinderAvatarView.kt
@@ -1,13 +1,45 @@
 package com.tinder.ui.core
 
 import android.content.Context
+import android.graphics.RectF
 import android.util.AttributeSet
-import android.widget.FrameLayout
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.shape.*
+import kotlin.math.max
+import kotlin.math.min
 
 /**
  * Circular ImageView, used for displaying user avatars etc.
- * TODO: fill this view out
  */
 class TinderAvatarView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) :
-    FrameLayout(context, attrs) {
+    ShapeableImageView(context, attrs) {
+
+    init {
+        if (height != width) {
+            layoutParams = layoutParams.apply { height = width }
+        }
+
+        shapeAppearanceModel =
+            ShapeAppearanceModel().toBuilder()
+                .setBottomLeftCorner(CornerFamily.ROUNDED, ::halfWidth)
+                .setBottomRightCorner(CornerFamily.ROUNDED, ::halfWidth)
+                .setTopLeftCorner(CornerFamily.ROUNDED, ::halfWidth)
+                .setTopRightCorner(CornerFamily.ROUNDED, ::halfWidth)
+                .build()
+    }
+
+    // Force the smaller dimension to make sure we stay a circle despite
+    // any developer mistake in XML etc.
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val h = this.measuredHeight
+        val w = this.measuredWidth
+        var size = min(w, h)
+        if (size == 0) size = max(w, h)
+        setMeasuredDimension(size, size)
+    }
+
+    private fun halfWidth(bounds: RectF) : Float {
+        return bounds.width() * 0.5f
+    }
 }
